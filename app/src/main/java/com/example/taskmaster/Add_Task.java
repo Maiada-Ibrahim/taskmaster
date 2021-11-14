f0811083ee2cfef46cdeec147a622b4dd6690e75
maiada-ibrahim <maiada.ibrahim.27@icloud.com>
2021-11-14 19:19:17 +0200

    finish

---
+++ app/src/main/java/com/example/taskmaster/Add_Task.java
@@ -0,0 +1,24 @@
+package com.example.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Add_Task extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+
+        Button addTaskP2=findViewById(R.id.addTaskP2);
+        addTaskP2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "submitted!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button addTask=findViewById(R.id.addTask);
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(), "Button Clicked", Toast.LENGTH_LONG).show();
+                Intent goToAddTask = new Intent(MainActivity.this, Add_Task.class);
+//                goToAddTask.putExtra("passedValue", "hello");
+                startActivity(goToAddTask);
+            }
+        });
+
+
+        Button allTask=findViewById(R.id.allTask);
+        allTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToallTask = new Intent(MainActivity.this, all_Task.class);
+                startActivity(goToallTask);
+            }
+        });
+
     }
+
+
+
 }
