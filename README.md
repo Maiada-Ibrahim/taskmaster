5a6c4b8435de5c32c3e50b7352036f265d74aaef
maiada-ibrahim <maiada.ibrahim.27@icloud.com>
2021-11-15 17:00:28 +0200

    finishlab-27

--- README.md
+++ README.md
@@ -8,5 +8,15 @@ in this lab we created a three pages:
 - All Tasks page: in this page nothing added till now.
 
   
-![](./screenshots/lab26.png)
+<!-- ![](./screenshots/lab26.png) -->
 ![](lab26.png)
+
+-------------------------------------------------------------------------------
+# Lab: 27:
+in this lab we created new two pages 
+- task details: include a title, and body with Lorem Ipsum content 
+
+- setting page: that accept the user name and render it to the homepage.
+![](lab27.png)
+
+-----------------------------------------------------------------------------------------
--- app/src/main/java/com/example/taskmaster/Detail.java
+++ app/src/main/java/com/example/taskmaster/Detail.java
@@ -3,6 +3,7 @@ package com.example.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Detail extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class Detail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+
+        String TextTitle= getIntent().getStringExtra("detail");
+        TextView titleholder = findViewById(R.id.detailTitle);
+        titleholder.setText(TextTitle);
+
     }
 }
